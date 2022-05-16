43863a3386e1ef2a0230d52aa1ba99f4c73d49fc
Vlad <vvllaadd1233345@gmail.com>
2022-05-16 20:08:43 +0200

    feat: endpoint for statictic (filter transactions)

--- controllers/transactions/index.js
+++ controllers/transactions/index.js
@@ -1,7 +1,9 @@
 const getTransactions = require('./getTransactions');
 const addTransaction = require('./addTransaction');
+const getFilterTransactions = require('./getFilterTransactions');
 
 module.exports = {
   getTransactions,
   addTransaction,
-}
+  getFilterTransactions,
+};
